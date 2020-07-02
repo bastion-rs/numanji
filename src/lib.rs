9615f1bbed621ad6bb9428fe3d9400c06a076782
o0Ignition0o <jeremy.lempereur@gmail.com>
2020-07-02 16:07:29 +0200

    Clippy fixes

--- src/lib.rs
+++ src/lib.rs
@@ -23,10 +23,8 @@
 //! // Do autoselect for allocator
 //! autoselect!();
 //!
-//! fn main() {
-//!     // Allocated by Numanji based on your Numa availability on your system.
-//!     let _vec = Vec::<usize>::with_capacity(1234);
-//! }
+//! // Allocated by Numanji based on your Numa availability on your system.
+//! let _vec = Vec::<usize>::with_capacity(1234);
 //! ```
 //!
 
